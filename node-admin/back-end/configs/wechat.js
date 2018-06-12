f92d5b1d3f9450b47b8539b263afc89cdd498e46
MChao <mrzzchao@foxmail.com>
2018-06-12 12:45:50 +0800

    更改微信小程序配置

--- node-admin/back-end/configs/wechat.js
+++ node-admin/back-end/configs/wechat.js
@@ -1,6 +1,6 @@
 // 小程序信息
 module.exports = {
-    appid: 'wx93cc364dffcadf74',
-    secret: 'ecbaa29188eb3324115561551382fb63',
+    appid: 'wxd3dc21189a79e3cc',
+    secret: '48d916adf470ccd1e022df8de8201768',
     grant_type: 'authorization_code'
 }
